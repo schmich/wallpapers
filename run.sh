451139ee0ed7a183fdb210733173044c3ef2109b
Chris Schmich <schmch@gmail.com>
2016-06-16 00:17:54 -0500

    Initial commit.

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+path=`curl -s http://www.bing.com | grep -m1 -oP 'url:\s*("[^"]+")' | cut -d: -f2 | jq '.' | tr -d '"'`
+url=http://www.bing.com$path
+
+file=`echo $url | rev | cut -d/ -f1 | rev`
+out=wallpapers/$file
+
+if [ -f "$out" ]; then
+  echo "Wallpaper already exists, exiting."
+  exit -1
+fi
+
+mkdir -p wallpapers
+curl -s -o "$out" "$url"
+
+git checkout -b wallpapers
+git add -v wallpapers
+git commit -m "Add wallpaper."
+ssh-agent bash -c "ssh-add id_rsa && git push -u origin wallpapers"
